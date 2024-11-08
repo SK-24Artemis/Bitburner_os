114dc0a7e386bfa67ad6abc32dfdbe02c05e023a
SK_Artemis <98838698+SK3Artemis@users.noreply.github.com>
2024-11-08 01:33:14 +0100

    Update DevMenu.js

--- DevMenu.js
+++ DevMenu.js
@@ -9,14 +9,3 @@ export async function main(ns) {
   const Router = Object.values(RouterModule).find(obj => obj.toPage !== undefined);
   Router.toPage("Dev")
 }
-export function getRequire() {
-
-  const chunk = webpackChunkbitburner;
-
-  if (!chunk.some(m => m.some(i => i[0] == -1))) {
-    let requireCache;
-    chunk.push([[-1], {}, r => (requireCache = requireCache ?? r, requireCache)]);
-  }
-
-  return chunk.find(m => m.some(i => i[0] == -1))[2]();
-}
